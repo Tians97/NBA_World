7514d1ede014f5e2cb47463c43fe02ffa976dae2
Tianshu Xiao <tx2@g.ucla.edu>
2022-08-15 22:01:06 -0700

    map

--- src/index.js
+++ src/index.js
@@ -1,8 +1,30 @@
 
 import {map} from './scripts/map'
 
+const title = document.getElementsByClassName('title')[0]
+const body = document.getElementsByClassName('modal-body')[0]
+
 document.addEventListener('DOMContentLoaded', function(){
+    let backMap = d3.select("#nba-map")
+
+    backMap.on("click", (e)=>{
+        console.log(e.target.id)
+        console.log(e.target.class)
+        if(e.target.nodeName === "image"){
+            modal.style.display = "block"
+            title.innerHTML = e.target.id
+            body.innerHTML = e.target
+        }
+    })
+
+    const closeModalButtons = document.querySelectorAll('.close-button')
+
+    closeModalButtons.forEach(button => {
+        button.addEventListener('click', () => {
+            const modal = button.closest('.modal')
+            modal.style.display = "none"
+        })
+    })
+    
     map();
 })
-
-
